98cc89d07dc28343e34097e9c0963c157a753de9
Fabio Lopez <andresviveros1984@googlemail.com>
2023-11-25 11:22:52 +0000

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <p>home</p>
+        </div>
+    )
+    //create nav using material ui, put it on the home page and setup reat router.
+    // after that try to creat a login butoon using auth0 and put it on the right of nav bar.
+}
+
+
+
+export default Home;
