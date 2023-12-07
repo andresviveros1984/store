90e323c5df3a4e65921b7136fdb8891f60acdc64
Fabio Lopez <andresviveros1984@googlemail.com>
2023-12-07 10:04:10 +0000

    add product details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import Product from './Product';
+import React from "react";
+import styled from "styled-components";
+import Product from "./Product";
+import { Box, Container } from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
+import CircularProgress from "@mui/material/CircularProgress";
 
-
-
-const Home = ({results}) => {
-    return (
-        <div>
-            <p>Products</p>
-            {results.products.map(result =>{
-                return(
-                    <Product product={result}/>
-                )
-            })}
-            
-        </div>
-    )
-
-    
-}
-
-
+const Home = ({ results }) => {
+  return (
+    <Container>
+      {results.products ? (
+        <Box>
+          <p>Products</p>
+          {results.products.map((result) => {
+            return <Product product={result} />;
+          })}
+        </Box>
+      ) : (
+        <Box sx={{ display: "flex", width: "100vw", height: "100vh" ,alignItems:"center", justifyContent:'center'}}>
+          <CircularProgress />
+        </Box>
+      )}
+    </Container>
+  );
+};
 
 export default Home;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import Typography from '@mui/material/Typography'
-
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Card, CardMedia, CardContent, CardActions,IconButton } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 //display some products
 //use map or for loop
 //display categories as a menu option, side bar on burger menu
-const Product = ({product}) => {
+const Product = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: "350px", bgcolor: "red" }}>
+      <CardMedia component="img" title="" image={product.images[0]} />
+      <CardContent>
+        <Typography variant="inherit" color="initial">
+          {product.title}
+        </Typography>
+        <Typography variant="inherit" color="initial">
+          {product.description}
+        </Typography>
+        <Typography variant="inherit" color="initial">
+          $ {product.price}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+    </Card>
+  );
+};
 
-    return(
-        <div>
-            <Typography variant="h1" color="initial">{product.title}</Typography>
-        </div>
-    )
-}
-
-export default Product
+export default Product;
