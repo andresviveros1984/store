25bbcf82fa86892c62128dd549b7b2737a395fd2
Fabio Lopez <andresviveros1984@googlemail.com>
2024-02-02 19:18:04 +0000

    resize images

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -6,6 +6,7 @@ import { Box, Container, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import CircularProgress from "@mui/material/CircularProgress";
 
 const ProductDetails = () => {
 
@@ -13,6 +14,8 @@ const ProductDetails = () => {
 
     const [productDetail, setProductDetail] = useState({});
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [mainImage,setMainImage] = useState('');
 
     const getProductDetails = async () => {
         try {
@@ -23,9 +26,11 @@ const ProductDetails = () => {
 
             const data = await response.json();
             setProductDetail(data);
+            setMainImage(data.images[0]);
         } catch (error) {
             setError(error.toString());
         }
+
 
     }
 
@@ -35,31 +40,34 @@ const ProductDetails = () => {
 
     return (
         <Box>
-            <Grid container>
-                {error && (<Typography>{error}</Typography>)}
-                <Grid item xs={12} md={6}>
-                    <Grid container>
-                        <Grid item md={3}>
-                            <Grid container>
-                                {productDetail && productDetail.images.map(image => {
-                                    return (
-                                        <Grid item md={10}>
-                                            <img src={image} alt="" />
-                                        </Grid>
-                                    )
-                                })}
+            {console.log(productDetail)}
+            {!productDetail.images ? <Box sx={{ display: "flex", width: "100vw", height: "100vh", alignItems: "center", justifyContent: 'center' }}>
+                <CircularProgress />
+            </Box> : (
+                <Grid container>
+                    {error && (<Typography>{error}</Typography>)}
+                    <Grid item  md={6}>
+                        <Grid container>
+                            <Grid item md={3}>
+                                <Grid container mx={"10px"} my={"10px"}>
+                                    {productDetail.images.map(image => {
+                                        return (
+                                            <Grid border={"1px solid red"} item md={10} onClick={()=> setMainImage(image)}>
+                                                <img src={image} alt=""  width={130} height={100}/>
+                                            </Grid>
+                                        )
+                                    })}
+                                </Grid>
+                            </Grid>
+                            <Grid item md={9} my={"10px"} border={"1px solid red"}>
+                                <img src={mainImage} alt="" width={500} height={530} />
                             </Grid>
                         </Grid>
-                        <Grid item md={9}>
-                            <img src={productDetail.thumbnail} alt="" />
-                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid item md={6}>
-                    Product details text
-                </Grid>
-            </Grid>
-
+                    <Grid item md={6} border={"1px solid red"} my={"10px"}>
+                        Product details text
+                    </Grid>
+                </Grid>) }
         </Box >
     )
     //create page for product detail
