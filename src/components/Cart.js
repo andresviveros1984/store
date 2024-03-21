db561c48b72bf2344b799ef07a3d71d3b826c11c
Fabio Lopez <andresviveros1984@googlemail.com>
2024-03-21 09:50:33 +0000

    update quantity icons

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Typography from '@mui/material/Typography'
-import { Box, Button, List, ListItem, ListItemText, Avatar, ListItemAvatar } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemText, Avatar, ListItemAvatar, IconButton } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UseNumberInputCompact from './UseNumberInputCompact';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
 //for a single user only, specific cart for logged in user
 
@@ -16,27 +18,28 @@ import UseNumberInputCompact from './UseNumberInputCompact';
 
 //create ui
 //create functions for handling increase in quantity
-const Cart = ({ cartItems, setCartItems,setCartCount,cartCount }) => {
+const Cart = ({ cartItems, setCartItems, setCartCount, cartCount }) => {
 
     const [quantity, setQuantity] = useState(0);
-    const [deleteItem,setDeleteItem] = useState(false);
+    const [deleteItem, setDeleteItem] = useState(false);
 
 
     const handleRemoveFromItemCart = (item) => {
         const remainingCartItems = [];
-        if(cartItems){
+        if (cartItems) {
             for (let i = 0; i < cartItems.length; i++) {
-                if(item.productID === cartItems[i].productID){
+                if (item.productID === cartItems[i].productID) {
                     continue;
-                }else{
+                } else {
                     remainingCartItems.push(cartItems[i]);
-                } 
+                }
             }
         }
+
+        //change this to use filter()
         setCartItems(remainingCartItems);
-        if(cartItems.length == 0){
-            setCartCount(cartCount = 0);
-        }
+        setCartCount(cartCount - item.quantity);
+
         //how to update the cart counter?? 
     }
 
@@ -54,12 +57,24 @@ const Cart = ({ cartItems, setCartItems,setCartCount,cartCount }) => {
                 {cartItems.map(item => {
                     return (
                         <List>
-                            <ListItem sx={{ pr: "50px"}}>
+                            <ListItem sx={{ pr: "50px" }}>
                                 <img src={item.image} style={{ width: "100px" }} />
                                 <ListItemText primary={item.name} sx={{ pl: "5px", width: "90px" }} />
                                 <ListItemText primary={"£" + item.price} />
-                                <UseNumberInputCompact quantity={item.quantity}/>
-                                <DeleteIcon onClick={()=>handleRemoveFromItemCart(item)} />  
+                                {/* <UseNumberInputCompact quantity={item.quantity}/> */}
+                                <Box sx={{display:"flex", height:"30px",alignItems:"center"}}>
+                                    <IconButton >
+                                        <AddCircleOutlineIcon />
+                                    </IconButton>
+                                    <Typography variant='inherit'>{item.quantity}</Typography>
+                                    <IconButton>
+                                        <RemoveCircleOutlineIcon/>
+                                    </IconButton>
+                                    {/* {create function one or two to handle increase and decrease
+                                    ,once quantity is zero in ui, remove cart item
+                                    } */}
+                                </Box>
+                                <DeleteIcon onClick={() => handleRemoveFromItemCart(item)} />
                             </ListItem>
                             <Divider />
                         </List>
